f1a9cd6cf04bbd7814e4d3796553b0203ab496de
Miyashirou <112007560+Miyashirou@users.noreply.github.com>
2024-03-23 22:52:27 +0800

    Add files via upload

---
+++ Percobaan1PAA/Percobaan1PAA/Helper/sqlDBhelper.cs
@@ -0,0 +1,33 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace Percobaan1PAA.Helper
+{
+    public class sqlDBhelper
+    {
+        private NpgsqlConnection connection;
+        private string __constr;
+
+        public sqlDBhelper(string pCOnstr)
+        {
+            __constr = pCOnstr;
+            connection = new NpgsqlConnection();
+            connection.ConnectionString = __constr;
+        }
+
+        public NpgsqlCommand getNpgsqlCommand(string query)
+        {
+            connection.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = query;
+            cmd.CommandType = CommandType.Text;
+            return cmd;
+        }
+
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+    }
+}
